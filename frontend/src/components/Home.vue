8f04b909c224a95e79f8bd68b5a093b7c003de17
Hamdi-korreshi <hamdikorreshi256@gmail.com>
2025-02-26 06:18:57 +0000

    Everything is up and running for docker, will declare and fix things after I get some other work done.

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>Welcome to My Vue + Django App</h1>
+    <p>{{ message }}</p>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const message = ref('')
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/hello/')
+    message.value = response.data.message
+  } catch (error) {
+    console.error(error)
+  }
+})
+</script>
