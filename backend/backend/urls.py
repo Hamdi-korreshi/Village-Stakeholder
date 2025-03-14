c10cf7531bf10ceedf5eba0959106fd9d73ef485
Hamdi-korreshi <hamdikorreshi256@gmail.com>
2025-03-14 00:30:30 +0000

    docker not syncing

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -16,12 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from api.registration_view import register_user
+from ap.registration_view import register_user
 from api.auth_views import get_csrf_token, signin, signout
 from api.views import random_string
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('village/v1/register', register_user, name='register')
     path('village/v1/csrf/', get_csrf_token, name='get_csrf_token'),
     path('village/v1/login', signin, name='signin'),
     path('village/v1/logout', signout, name='signout'),
