6ff04034d7c10c7e62e6dba6ac40754b685b36ca
rzVance <rzv3@njit.edu>
2025-03-27 07:06:04 +0000

    fixed fe,be,db dependencies

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .auth_views import get_csrf_token, user_login, user_logout
+from .managing_api import personal_village_members, list_user_villages, get_village_participants, add_villager, remove_villager
 from .registration_view import register_user
 from .views import random_string
 
@@ -7,4 +8,11 @@ urlpatterns = [ path('register/', register_user, name='register_user'),
     path('csrf/', get_csrf_token, name='get_csrf_token'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='user_logout'),
-    path('random-string/', random_string, name='random_string')]
+    path('random-string/', random_string, name='random_string'),
+    
+    path('personal-village-members/', personal_village_members, name='personal_village_members'),
+    path('list-user-villages/', list_user_villages, name='list_user_villages'),
+    path('get-village-participants/<int:village_id>/', get_village_participants, name='get_village_participants'),
+    path('add-village/', add_villager, name='add_village'),
+    path('remove-village/<int:village_id>/', remove_villager, name='remove_village'),
+    ]
