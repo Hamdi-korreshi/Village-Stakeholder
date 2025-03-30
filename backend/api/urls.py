bb36bdfc7216eab377ae6d14dc0bfdbf3a19e1e8
rzVance <rzv3@njit.edu>
2025-03-30 05:01:31 +0000

    pulled main and fixed merge conflicts

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -10,16 +10,13 @@ urlpatterns = [ path('register/', register_user, name='register_user'),
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='user_logout'),
     path('random-string/', random_string, name='random_string'),
-<<<<<<< HEAD
-    
-    
+    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
+
+    # Villager management
     path('new-village/', initialize_village, name='initialize_village'),
     path('personal-village-members/', personal_village_members, name='personal_village_members'),
     path('list-user-villages/', list_user_villages, name='list_user_villages'),
     path('get-village-participants/<int:village_id>/', get_village_participants, name='get_village_participants'),
     path('add-village/', add_villager, name='add_village'),
     path('remove-village/<int:village_id>/', remove_villager, name='remove_village'),
-=======
-    path('change-password/', ChangePasswordView.as_view(), name='change-password'),
->>>>>>> origin/main
     ]
